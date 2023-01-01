47fbd56e2bc8883d21959429acc291de47b27d96
Bishal Mahanta <bishalmahanta0.0.0@gmail.com>
2023-01-01 15:14:41 +0530

    make checkForWin function

--- src/main.c
+++ src/main.c
@@ -21,6 +21,8 @@ int gameData[9] = {[0 ... 8] = -1};
 // activePlayer will be used to keep track of the player playing the game
 int activePlayer = 1;
 int playerScores[2][5] = {};
+int winCondition[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6},
+                          {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
 /**
  * @brief Prints empty lines to console
@@ -38,54 +40,24 @@ int printNewLines(int);
  */
 bool drawBoard(void);
 
+/**
+ * @brief Mark Tic Tac Toe Board and update player scores
+ *
+ * @return true
+ * @return false
+ */
+bool markBoard(void);
+
+/**
+ * @brief Check if the game has been won
+ *
+ * @return int activePlayer
+ */
+int checkForWin(void);
+
 // ANCHOR Main Function
 int main(int argc, char *argv[]) {
-  system("clear");
-
-  /* // ANCHOR - Print gameData
-  for (int i = 0; i < 9; i++) {
-    printf("%d ", gameData[i]);
-  }
-  printNewLines(1);
-
-  // ANCHOR - Print playerScore
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 5; j++) {
-      printf("%d ", playerScores[i][j]);
-    }
-    printNewLines(1);
-  } */
-
-  int winCondition[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6},
-                            {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-
-  int testArr[2][5] = {{1, 2, 3, 4, 0}, {4, 5, 6, 7, 8}};
-  int counter = 0;
-
-  // printf("testArr: %d", testArr[0][i]);
-  // printNewLines(2);
-  bool win = false;
-
-  for (int j = 0; j < 8 && win == false; j++) {
-      counter = 0;
-    for (int i = 0; i < 5 && win == false; i++) {
-      for (int k = 0; k < 3; k++) {
-        if (testArr[0][i] == winCondition[j][k])
-          counter++;
-        // printf("winCondition: %d\t", winCondition[j][k]);
-        printf("i = %d\tj = %d\tk = %d\ttestArr = %d\t winCondition = %d\t", i, j, k, testArr[0][i], winCondition[j][k]);
-        printf("counter: %d\n", counter);
-        if (counter == 3) {
-          printf("Win");
-          printNewLines(1);
-          win = true;
-          break;
-        }
-        // printNewLines(1);
-      }
-      // printNewLines(1);
-    }
-  }
+  system("clear");  
 
   return true;
 }
@@ -116,4 +88,45 @@ bool drawBoard(void) {
   // ANCHOR drawBoard
 
   return true;
+}
+
+/**
+ * @brief Mark Tic Tac Toe Board and update player scores
+ *
+ * @return true
+ * @return false
+ */
+bool markBoard(void) {
+  // ANCHOR markBoard
+
+  return true;
+}
+
+/**
+ * @brief Check if the game has been won
+ * 
+ * @return true
+ * @return false
+ */
+int checkForWin(void) {
+  // ANCHOR checkForWin
+  bool win = false;
+  int counter = 0;
+
+  for (int j = 0; j < 8 && win == false; j++) {
+    counter = 0;
+    for (int i = 0; i < 5 && win == false; i++) {
+      for (int k = 0; k < 3; k++) {
+        if (playerScores[activePlayer][i] == winCondition[j][k])
+          counter++;
+        if (counter == 3) {
+          printf("Win");
+          printNewLines(1);
+          win = true;
+          break;
+        }
+      }
+    }
+  }
+  return activePlayer;
 }
